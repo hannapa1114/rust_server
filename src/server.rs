2820a36c605b323f834cd26edfff9bd40f658e13
inspire <inspire@MIN-MBK-8b1aa6.local>
2024-04-16 14:03:37 +0900

    match 구문으로 TCP 소켓 연결 및 버퍼 받기

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,4 @@
-use std::net::TcpListener;
+use std::{io::Read, net::TcpListener};
 
 pub struct Server {
     addr: String,
@@ -12,7 +12,24 @@ impl Server {
     pub fn run(self) {
         println!("Listening on {}", self.addr);
 
-        // Result Enum 
+        // Result Enum
         let listner = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            match listner.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024]; // 서버가 받을 수 있는 Result 버퍼 크기 설정
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Recieved: {}", String::from_utf8_lossy(&buffer))
+                        }
+                        Err(e) => println!("Failed to connection: {}", e),
+                    }
+                }
+                Err(e) => {
+                    print!("Failed to connection :{}", e)
+                }
+            }
+        }
     }
 }
